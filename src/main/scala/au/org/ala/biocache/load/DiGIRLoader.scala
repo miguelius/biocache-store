5a06f202764b9792e719fa41d7551beb65841235
davidbairdala <david.baird.ala@gmail.com>
2014-07-24 15:48:29 +1000

    First cut at pushing DiGIR records to cassandra

--- src/main/scala/au/org/ala/biocache/load/DiGIRLoader.scala
+++ src/main/scala/au/org/ala/biocache/load/DiGIRLoader.scala
@@ -1,43 +1,31 @@
 package au.org.ala.biocache.load
 
 import java.net.URI
-import java.util.Date
-import java.util.UUID
+import java.util
+import java.util.{Date, UUID}
 import java.util.concurrent.TimeUnit
-import org.apache.commons.httpclient.params.HttpConnectionParams
-import org.apache.http.client.params.ClientPNames
-import org.apache.http.conn.scheme.PlainSocketFactory
-import org.apache.http.conn.scheme.Scheme
-import org.apache.http.conn.scheme.SchemeRegistry
-import org.apache.http.impl.client.DecompressingHttpClient
-import org.apache.http.impl.client.DefaultHttpClient
-import org.apache.http.impl.conn.PoolingClientConnectionManager
-import org.apache.http.params.BasicHttpParams
-import org.gbif.crawler.AbstractCrawlListener
-import org.gbif.crawler.CrawlConfiguration
-import org.gbif.crawler.CrawlContext
-import org.gbif.crawler.Crawler
-import scala.xml._ 
 
-import scala.collection.JavaConverters._
-
-import org.gbif.crawler.client.HttpCrawlClient
-import org.gbif.crawler.protocol.digir.DigirCrawlConfiguration
-import org.gbif.crawler.protocol.digir.DigirResponseHandler
-import org.gbif.crawler.protocol.digir.DigirScientificNameRangeRequestHandler
-import org.gbif.crawler.retry.LimitedRetryPolicy
-import org.gbif.crawler.strategy.ScientificNameRangeCrawlContext
-import org.gbif.crawler.strategy.ScientificNameRangeStrategy
-import org.gbif.wrangler.lock.NoLockFactory
-import org.slf4j.LoggerFactory
-import org.slf4j.MDC
-import com.google.common.base.Optional
-import com.google.common.primitives.Bytes
 import au.org.ala.biocache.cmd.Tool
-import au.org.ala.biocache.model.FullRecord
 import au.org.ala.biocache.model.Versions
 import au.org.ala.biocache.util.OptionParser
-import com.google.protobuf.ByteString
+import com.google.common.base.Optional
+import com.google.common.primitives.Bytes
+import org.apache.commons.httpclient.params.HttpConnectionParams
+import org.apache.commons.lang.StringUtils
+import org.apache.http.client.params.ClientPNames
+import org.apache.http.conn.scheme.{PlainSocketFactory, Scheme, SchemeRegistry}
+import org.apache.http.impl.client.{DecompressingHttpClient, DefaultHttpClient}
+import org.apache.http.impl.conn.PoolingClientConnectionManager
+import org.apache.http.params.BasicHttpParams
+import org.gbif.crawler.{AbstractCrawlListener, CrawlConfiguration, CrawlContext, Crawler}
+import org.gbif.crawler.client.HttpCrawlClient
+import org.gbif.crawler.protocol.digir.{DigirCrawlConfiguration, DigirResponseHandler, DigirScientificNameRangeRequestHandler}
+import org.gbif.crawler.retry.LimitedRetryPolicy
+import org.gbif.crawler.strategy.{ScientificNameRangeCrawlContext, ScientificNameRangeStrategy}
+import org.gbif.wrangler.lock.NoLockFactory
+import org.slf4j.{LoggerFactory, MDC}
+
+import scala.xml._
 
 object DiGIRLoader extends Tool {
 
@@ -45,7 +33,7 @@ object DiGIRLoader extends Tool {
   def desc = "Load DiGIR."
 
   def main(args: Array[String]) {
-    val LOG = LoggerFactory.getLogger(getClass());
+    val LOG = LoggerFactory.getLogger(getClass)
 
     var dataResourceUid = ""
     var updateLastChecked = true
@@ -67,7 +55,7 @@ object DiGIRLoader extends Tool {
           l.updateLastChecked(dataResourceUid)
         }
       } catch {
-        case e: Exception => e.printStackTrace
+        case e: Exception => e.printStackTrace()
       } finally {
       }
 
@@ -76,7 +64,7 @@ object DiGIRLoader extends Tool {
 }
 
 class DiGIRLoader extends DataLoader {
-  val LOG = LoggerFactory.getLogger(getClass());
+  val LOG = LoggerFactory.getLogger(getClass)
 
   def load(dataResourceUid: String, test: Boolean) {
     val (protocol, urls, uniqueTerms, params, customParams, lastChecked) = retrieveConnectionParameters(dataResourceUid)
@@ -91,16 +79,15 @@ class DiGIRLoader extends DataLoader {
     val manis = false // gbif specific, not used
     val config = new DigirCrawlConfiguration(gbifID, attempt, endpoint, code, manis)
     val context = new ScientificNameRangeCrawlContext()
-    val strategy = new ScientificNameRangeStrategy(context);
+    val strategy = new ScientificNameRangeStrategy(context)
 
-    val retryPolicy = new LimitedRetryPolicy(5, 2, 5, 2);
-    val requestHandler = new DigirScientificNameRangeRequestHandler(config);
+    val retryPolicy = new LimitedRetryPolicy(5, 2, 5, 2)
+    val requestHandler = new DigirScientificNameRangeRequestHandler(config)
 
-    val client = HttpCrawlClientProvider.newHttpCrawlClient();
+    val client = HttpCrawlClientProvider.newHttpCrawlClient()
 
-    val crawler =
-      Crawler.newInstance(strategy, requestHandler, new DigirResponseHandler(), client, retryPolicy,
-        NoLockFactory.getLock())
+    val crawler = Crawler.newInstance(strategy, requestHandler, new DigirResponseHandler(), client, retryPolicy, NoLockFactory.getLock)
+
     val emit = (record: Map[String, String]) => {
       LOG.info(f"$record")
       val fr = FullRecordMapper.createFullRecord("", record, Versions.RAW)
@@ -109,7 +96,7 @@ class DiGIRLoader extends DataLoader {
     	  if (load( dataResourceUid, fr, uniqueTerms)) {
     	    LOG.info(f"$dataResourceUid stored successfully")
     	  } else {
-    		LOG.info(f"Problems storing $dataResourceUid")
+    		  LOG.info(f"Problems storing $dataResourceUid")
     	  }
       }
     }
@@ -117,12 +104,12 @@ class DiGIRLoader extends DataLoader {
     crawler.addListener(new LoggingCrawlListener(config, null, null, 0, null).asInstanceOf[org.gbif.crawler.CrawlListener[ScientificNameRangeCrawlContext, String, java.util.List[java.lang.Byte]]])
     crawler.crawl()
 
-    println("done")
+    LOG.info("Done.")
   }
 }
 
 class AlaBiocacheListener(test: Boolean, emit: (Map[String, String]) => Unit) extends AbstractCrawlListener[ScientificNameRangeCrawlContext, String, java.util.List[java.lang.Byte]] {
-  val LOG = LoggerFactory.getLogger(getClass());
+  val LOG = LoggerFactory.getLogger(getClass)
 
   override def response(
     response: java.util.List[java.lang.Byte],
@@ -131,20 +118,19 @@ class AlaBiocacheListener(test: Boolean, emit: (Map[String, String]) => Unit) ex
     recordCount: Optional[java.lang.Integer],
     endOfRecords: Optional[java.lang.Boolean]): Unit = {
 
-    LOG.info(f"recordCount: ${recordCount}, endOfRecords: ${endOfRecords}")
+    LOG.info(f"recordCount: $recordCount, endOfRecords: $endOfRecords")
     if (0 < recordCount.get()) {
 	    val xmlResponseAsString = new String(Bytes.toArray(response))
 	    val xmlResponse = XML.loadString(xmlResponseAsString)
 	    val records = xmlResponse \\ "record"
-	    val map = Map("scientificName" -> "macropus rufus")
-	    emit(map)
 
-	    //records.map(f)
-	    if (!test) {
-	      // do your work
-	    }
+	    records.map((record) => {
+        val fieldMap = record.child.filter((node: Node) => { node.prefix == "darwin" && StringUtils.isNotEmpty(node.text) }).map( (x:Node) => { (x.label, x.text) }).toMap[String, String]
+        emit(fieldMap)
+      })
+
     }
-    LOG.info("Received: {}", new String(Bytes.toArray(response)));
+    LOG.info("Received: {}", new String(Bytes.toArray(response)))
   }
 
 }
@@ -158,40 +144,40 @@ class LoggingCrawlListener(
 
   var startDate: java.util.Date) extends AbstractCrawlListener[ScientificNameRangeCrawlContext, String, java.util.List[java.lang.Byte]] {
 
-  val LOG = LoggerFactory.getLogger(getClass());
+  val LOG = LoggerFactory.getLogger(getClass)
 
-  MDC.put("datasetKey", configuration.getDatasetKey().toString());
-  MDC.put("attempt", String.valueOf(configuration.getAttempt()));
+  MDC.put("datasetKey", configuration.getDatasetKey.toString)
+  MDC.put("attempt", String.valueOf(configuration.getAttempt))
 
   override def error(msg: String) {
-    LOG.warn("error during crawling: [{}], last request [{}], message [{}]", lastContext, lastRequest, msg);
+    LOG.warn("error during crawling: [{}], last request [{}], message [{}]", lastContext, lastRequest, msg)
   }
 
   override def error(e: Throwable) {
-    LOG.warn("error during crawling: [{}], last request [{}]", lastContext, lastRequest, e);
+    LOG.warn("error during crawling: [{}], last request [{}]", lastContext, lastRequest, e)
   }
 
   override def finishCrawlAbnormally() {
-    finishCrawl(FinishReason.ABORT);
+    finishCrawl(FinishReason.ABORT)
   }
 
   override def finishCrawlNormally() {
-    finishCrawl(FinishReason.NORMAL);
+    finishCrawl(FinishReason.NORMAL)
   }
 
   override def finishCrawlOnUserRequest() {
-    finishCrawl(FinishReason.USER_ABORT);
+    finishCrawl(FinishReason.USER_ABORT)
   }
 
   override def progress(context: ScientificNameRangeCrawlContext) {
-    lastRequest = null;
-    lastContext = context;
-    LOG.info(f"now beginning to crawl [${context}]");
+    lastRequest = null
+    lastContext = context
+    LOG.info(f"now beginning to crawl [$context]")
   }
 
   override def request(req: String, retry: Int) {
-    LOG.info(f"requested page for [${lastContext}], retry [${retry}], request [${req}]");
-    lastRequest = req;
+    LOG.info(f"requested page for [$lastContext], retry [$retry], request [$req]")
+    lastRequest = req
   }
 
   override def response(
@@ -200,24 +186,24 @@ class LoggingCrawlListener(
     duration: Long,
     recordCount: Optional[java.lang.Integer],
     endOfRecords: Optional[java.lang.Boolean]): Unit = {
-    totalRecordCount += recordCount.or(0);
+    totalRecordCount += recordCount.or(0)
     val took = TimeUnit.MILLISECONDS.toSeconds(duration)
-    LOG.info(f"got response for [${lastContext}], records [${recordCount}], endOfRecords [${endOfRecords}], retry [${retry}], took [${took}s]")
+    LOG.info(f"got response for [$lastContext], records [$recordCount], endOfRecords [$endOfRecords], retry [$retry], took [${took}s]")
   }
 
   override def startCrawl() {
-    this.startDate = new Date();
-    LOG.info("started crawl");
+    this.startDate = new Date()
+    LOG.info("started crawl")
   }
 
   def finishCrawl(reason: FinishReason.Value) {
-    val finishDate = new Date();
-    val minutes = (finishDate.getTime() - startDate.getTime()) / (60 * 1000);
+    val finishDate = new Date()
+    val minutes = (finishDate.getTime - startDate.getTime) / (60 * 1000)
     LOG.info(
-      f"finished crawling with a total of [${totalRecordCount}] records, reason [${reason}], started at [${startDate}], finished at [${finishDate}], took [${minutes}] minutes");
+      f"finished crawling with a total of [$totalRecordCount] records, reason [$reason], started at [$startDate], finished at [$finishDate], took [$minutes] minutes")
 
-    MDC.remove("datasetKey");
-    MDC.remove("attempt");
+    MDC.remove("datasetKey")
+    MDC.remove("attempt")
   }
 
   object FinishReason extends Enumeration {
@@ -233,32 +219,33 @@ class LoggingCrawlListener(
 
 object HttpCrawlClientProvider {
 
-  val DEFAULT_HTTP_PORT = 80;
+  val DEFAULT_HTTP_PORT = 80
 
-  val CONNECTION_TIMEOUT_MSEC = 600000; // 10 mins
-  val MAX_TOTAL_CONNECTIONS = 500;
-  val MAX_TOTAL_PER_ROUTE = 20;
+  val CONNECTION_TIMEOUT_MSEC = 600000 // 10 mins
+  val MAX_TOTAL_CONNECTIONS = 500
+  val MAX_TOTAL_PER_ROUTE = 20
 
   def newHttpCrawlClient(): HttpCrawlClient = {
-    val schemeRegistry = new SchemeRegistry();
-    schemeRegistry.register(new Scheme("http", DEFAULT_HTTP_PORT, PlainSocketFactory.getSocketFactory()));
+    val schemeRegistry = new SchemeRegistry()
+    schemeRegistry.register(new Scheme("http", DEFAULT_HTTP_PORT, PlainSocketFactory.getSocketFactory))
 
-    val connectionManager = new PoolingClientConnectionManager(schemeRegistry);
-    connectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
-    connectionManager.setDefaultMaxPerRoute(MAX_TOTAL_PER_ROUTE);
+    val connectionManager = new PoolingClientConnectionManager(schemeRegistry)
+    connectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS)
+    connectionManager.setDefaultMaxPerRoute(MAX_TOTAL_PER_ROUTE)
 
-    val params = new BasicHttpParams();
+    val params = new BasicHttpParams()
     params.setParameter(HttpConnectionParams.CONNECTION_TIMEOUT, CONNECTION_TIMEOUT_MSEC)
     params.setParameter(HttpConnectionParams.SO_TIMEOUT, CONNECTION_TIMEOUT_MSEC)
-    params.setLongParameter(ClientPNames.CONN_MANAGER_TIMEOUT, CONNECTION_TIMEOUT_MSEC);
-    val httpClient = new DecompressingHttpClient(new DefaultHttpClient(connectionManager, params));
+    params.setLongParameter(ClientPNames.CONN_MANAGER_TIMEOUT, CONNECTION_TIMEOUT_MSEC)
+    val httpClient = new DecompressingHttpClient(new DefaultHttpClient(connectionManager, params))
 
-    return new HttpCrawlClient(connectionManager, httpClient);
+    new HttpCrawlClient(connectionManager, httpClient)
   }
 
   def HttpCrawlClientProvider() {
-    throw new UnsupportedOperationException("Can't initialize class");
+    throw new UnsupportedOperationException("Can't initialize class")
   }
+
 }
 
 
